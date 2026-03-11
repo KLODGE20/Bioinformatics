3140c22b12f06ebe8f3ba47a58b1562ab9729fbf
KLODGE20 <klodge2004@gmail.com>
2026-03-11 11:39:51 -0400

    push

---
+++ Lab_10/Lab_10_Script.R
@@ -0,0 +1,9 @@
+# install.packages("protti")
+# install.packages("UniprotR")
+# install.packages("r3dmol")
+
+library(Biostrings)
+library(GenomicAlignments)
+library(UniprotR)
+library(protti)
+library(r3dmol)
